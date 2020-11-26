a4bf24f6f745b5536107d3e28603761a7b92659d
xuxin <xuxin@sz-sunwin.com>
2020-11-26 13:20:49 +0800

    update mysql config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'parking', to: 'parking#index'
+  get 'contact', to: 'parking#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
